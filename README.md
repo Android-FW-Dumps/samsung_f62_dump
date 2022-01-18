7d75c11ee0ad7303a4ecd2f05ba4bf4aa37866b9
Sushrut1101 <guptasushrut@gmail.com>
2022-01-18 18:02:56 +0000

    Add extras for f62ins-user 11 RP1A.200720.012 E625FDDU2AUI2 release-keys
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+## f62ins-user 11 RP1A.200720.012 E625FDDU2AUI2 release-keys
+- Manufacturer: samsung
+- Platform: universal9825-r
+- Codename: f62
+- Brand: samsung
+- Flavor: f62ins-user
+- Release Version: 11
+- Id: RP1A.200720.012
+- Incremental: E625FDDU2AUI2
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: en-GB
+- Screen Density: undefined
+- Fingerprint: samsung/f62ins/f62:11/RP1A.200720.012/E625FDDU2AUI2:user/release-keys
+- OTA version: 
+- Branch: f62ins-user-11-RP1A.200720.012-E625FDDU2AUI2-release-keys
+- Repo: samsung_f62_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+cat recovery.img.* 2>/dev/null >> recovery.img
+rm -f recovery.img.* 2>/dev/null
+cat system/system/priv-app/SamsungCoreServices/SamsungCoreServices.apk.* 2>/dev/null >> system/system/priv-app/SamsungCoreServices/SamsungCoreServices.apk
+rm -f system/system/priv-app/SamsungCoreServices/SamsungCoreServices.apk.* 2>/dev/null
+cat system/system/priv-app/YourPhone_P1_5/YourPhone_P1_5.apk.* 2>/dev/null >> system/system/priv-app/YourPhone_P1_5/YourPhone_P1_5.apk
+rm -f system/system/priv-app/YourPhone_P1_5/YourPhone_P1_5.apk.* 2>/dev/null
+cat system/system/priv-app/LiveStickers/LiveStickers.apk.* 2>/dev/null >> system/system/priv-app/LiveStickers/LiveStickers.apk
+rm -f system/system/priv-app/LiveStickers/LiveStickers.apk.* 2>/dev/null
+cat system/system/priv-app/OneDrive_Samsung_v3/OneDrive_Samsung_v3.apk.* 2>/dev/null >> system/system/priv-app/OneDrive_Samsung_v3/OneDrive_Samsung_v3.apk
+rm -f system/system/priv-app/OneDrive_Samsung_v3/OneDrive_Samsung_v3.apk.* 2>/dev/null
+cat system/system/priv-app/AREmojiEditor/AREmojiEditor.apk.* 2>/dev/null >> system/system/priv-app/AREmojiEditor/AREmojiEditor.apk
+rm -f system/system/priv-app/AREmojiEditor/AREmojiEditor.apk.* 2>/dev/null
+cat system/system/priv-app/SecSettings/SecSettings.apk.* 2>/dev/null >> system/system/priv-app/SecSettings/SecSettings.apk
+rm -f system/system/priv-app/SecSettings/SecSettings.apk.* 2>/dev/null
+cat system/system/priv-app/AvatarEmojiSticker_Palette/AvatarEmojiSticker_Palette.apk.* 2>/dev/null >> system/system/priv-app/AvatarEmojiSticker_Palette/AvatarEmojiSticker_Palette.apk
+rm -f system/system/priv-app/AvatarEmojiSticker_Palette/AvatarEmojiSticker_Palette.apk.* 2>/dev/null
+cat system/system/priv-app/AREmoji/AREmoji.apk.* 2>/dev/null >> system/system/priv-app/AREmoji/AREmoji.apk
+rm -f system/system/priv-app/AREmoji/AREmoji.apk.* 2>/dev/null
+cat system/system/priv-app/Bixby/Bixby.apk.* 2>/dev/null >> system/system/priv-app/Bixby/Bixby.apk
+rm -f system/system/priv-app/Bixby/Bixby.apk.* 2>/dev/null
+cat system/system/app/HoneyBoard/HoneyBoard.apk.* 2>/dev/null >> system/system/app/HoneyBoard/HoneyBoard.apk
+rm -f system/system/app/HoneyBoard/HoneyBoard.apk.* 2>/dev/null
+cat system/system/apex/com.android.vndk.current.apex.* 2>/dev/null >> system/system/apex/com.android.vndk.current.apex
+rm -f system/system/apex/com.android.vndk.current.apex.* 2>/dev/null
+cat system/system/apex/com.android.art.release.apex.* 2>/dev/null >> system/system/apex/com.android.art.release.apex
+rm -f system/system/apex/com.android.art.release.apex.* 2>/dev/null
+cat product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> product/priv-app/Velvet/Velvet.apk
+rm -f product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> product/priv-app/GmsCore/GmsCore.apk
+rm -f product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat product/app/Maps/Maps.apk.* 2>/dev/null >> product/app/Maps/Maps.apk
+rm -f product/app/Maps/Maps.apk.* 2>/dev/null
+cat product/app/YouTube/YouTube.apk.* 2>/dev/null >> product/app/YouTube/YouTube.apk
+rm -f product/app/YouTube/YouTube.apk.* 2>/dev/null
+cat product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat product/app/Messages/Messages.apk.* 2>/dev/null >> product/app/Messages/Messages.apk
+rm -f product/app/Messages/Messages.apk.* 2>/dev/null
